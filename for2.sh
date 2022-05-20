b73f40a76a3216f6df7a7e051c5c81cb7e037c0c
kiritosshxd <96794616+kiritosshxd@users.noreply.github.com>
2022-05-20 01:08:13 -0300

    Create for2.sh

---
+++ for2.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+nums=$(seq 100 299 | tr -s '\n' '|')
+nums+=$(seq -s"|" 400 499)
+echo -e "Selecione o Range que deseja testar: "
+echo -e "[1] - 127.0.0.* "
+echo -e "[2] - 127.0.*.* "
+echo -e "[3] - 127.*.*.* "
+read -p "Opção: " x
+clear
+echo -e "Defina o ip pra teste (Ex: 127.0.0.): "
+read -p "Range de ip: " range
+read -p "Informe o DOMINIO: " domi
+read -p "Porta: " port
+clean
+case $x in
+1 | 01)
+    setup_op1
+    ;;
+2 | 02)
+    setup_op2
+    ;;
+3 | 03)
+    setup_op3
+    ;;
+*)
+    echo -e "\033[1;31mOpcao Invalida...\033[0m"
+    sleep 2
+    fun_conexao
+    ;;
+esac
+
+setup_op1() {
+    for i in {0..255}; do
+        try=$(curl -m 3 -s -o /dev/null -w "%{http_code}" $domi -H "Upgrade: websocket" -x ${range}${i}:$port)
+        ip2="${range}${i}"
+        eval "case $try in
+                 $nums\ )
+                      echo -e \"\e[01;33m$ip2\e[0m | \e[01;37mIP OK - STATUS $try\e[0m\"
+                      echo \"$ip2|$try\ | $date\">> OK.txt;;
+                   *)
+                      echo -e \"$ip2 | \e[01;33mSTATUS $try\e[0m\"
+           esac"
+
+    done
+}
+
+setup_op2() {
+    for q in {0..255}; do
+        for i in {0..255}; do
+            try=$(curl -m 3 -s -o /dev/null -w "%{http_code}" $domi -H "Upgrade: websocket" -x ${range}${q}.$i:$port)
+            ip2="${range}${q}.$i"
+            eval "case $try in
+                 $nums\ )
+                      echo -e \"\e[01;33m$ip2\e[0m | \e[01;37mIP OK - STATUS $try\e[0m\"
+                      echo \"$ip2|$try\ | $date\">> OK.txt;;
+                   *)
+                      echo -e \"$ip2 | \e[01;33mSTATUS $try\e[0m\"
+           esac"
+
+        done
+    done
+}
+
+setup_op3() {
+    for w in {0..255}; do
+        for q in {0..255}; do
+            for i in {0..255}; do
+                try=$(curl -m 3 -s -o /dev/null -w "%{http_code}" $domi -H "Upgrade: websocket" -x ${range}${w}.${q}.$i:$port)
+                ip2="${range}${w}.${q}.$i"
+                eval "case $try in
+                 $nums\ )
+                      echo -e \"\e[01;33m$ip2\e[0m | \e[01;37mIP OK - STATUS $try\e[0m\"
+                      echo \"$ip2|$try\ | $date\">> OK.txt;;
+                   *)
+                      echo -e \"$ip2 | \e[01;33mSTATUS $try\e[0m\"
+           esac"
+
+            done
+        done
+    done
+
+}
